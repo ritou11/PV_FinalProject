51ee019d775d0e6ae97c8463ab41c281d0060e2e
ritou11 <ritou11@gmail.com>
2017-05-30 11:03:58 +0800

    testSingle:umax umin

--- testSingle.m
+++ testSingle.m
@@ -1,3 +1,4 @@
+%% testSingle.m
 clear;
 Uoc = 335;
 Isc = 48;
@@ -5,29 +6,42 @@ Ump=240;
 Pmp=10e3;
 Imp=Pmp/Ump;
 [Iph, I0, N, Rs] = calcPvParameters(Uoc, Isc, Ump, Imp);
-Iph1=Iph;Iph2=Iph*0.3;
-Iph3=0.9*Iph;Iph4=0.1*Iph;
+
 U=(0:Uoc/100:Uoc)';
-IPH=0:0.1*Iph:Iph;
+IPH=0.1*Iph:0.05*Iph:2*Iph;
 I = zeros(length(U),length(IPH));
-h = waitbar(0,'Please wait...');
+h = waitbar(0,'Generating data...');
 steps = length(U)*length(IPH);
 for j=1:length(IPH)
     for i=1:length(U)
-        %I(i) = Pv4SeriesParallel(U(i),Iph1,Iph2,Iph3,Iph4,I0,N,Rs);
-        %I(i) = Pv2Series(U(i),Iph2,Iph3,I0,N,Rs);
         I(i,j) = PvFunctionI(U(i),IPH(j),I0,N,Rs);
         waitbar((i+(j-1)*length(U))/steps);
     end
 end
 close(h);
 %%
-figure;
+f = figure;
 plot(U/Uoc,U.*I(:,length(IPH))/max(U.*I(:,length(IPH))));
-hold on
-for i=1:length(IPH)-1
-    plot(U/Uoc,U.*I(:,i)/max(U.*I(:,i)));
+hold on;
+for i=1:length(IPH)
+    P(:,i)=U.*I(:,i);
+    plot(U/Uoc,P(:,i)/max(P(:,i)));
+    P(:,i)=P(:,i)/max(P(:,i));
 end
+[~,index]=max(P);
+umax = max(U(index))/Uoc;
+umin = min(U(index))/Uoc;
+plot(umax*ones(1,100),linspace(0,1,100),'k-.');
+plot(umin*ones(1,100),linspace(0,1,100),'k-.');
 xlim([0 1]);
 ylim([0 1]);
-grid on
+grid on;
+%%
+f = figure;
+hold on;
+plot(IPH/Iph,U(index)/Uoc);
+ylim([0 1]);
+plot(linspace(0,2,100),umax*ones(1,100),'k-.');
+grid on;
+grid minor;
+plot(linspace(0,2,100),umin*ones(1,100),'k-.');
