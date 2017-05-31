10025a946c244c3c40b25f4e41730805cf5ea7bf
ritou11 <ritou11@gmail.com>
2017-05-31 18:16:34 +0800

    generate report

---
+++ generateReport.m
@@ -0,0 +1,22 @@
+%% Generate Report
+clear;
+%% Task 1.1
+testSingle;
+testManySingle;
+%% Task 1.2
+testINC;
+% -fault testMPPTdynamic
+%% Task 1.3
+testMPPTdynamic;
+%% Task 1.4
+
+%% Task 2.1
+testMultiMaxCurve;
+testSingleMaxCurve;
+%% Task 2.2
+testMPPTfail;
+%% Task 2.3
+testRange;
+testMPPT;
+%% End
+clear
