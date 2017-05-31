9aa35bb0b3ba3229b056fcc747a87f5062012d21
ritou11 <ritou11@gmail.com>
2017-05-31 14:59:00 +0800

    inc figure

--- testINC.m
+++ testINC.m
@@ -1,4 +1,4 @@
-function simpleINC()
+%% testINC.m
 %% Parameters
 Uoc = 335;
 Isc = 48;
@@ -9,25 +9,32 @@ Imp=Pmp/Ump;
 Iph1=Iph;Iph2=Iph*0.3;
 Iph3=0.9*Iph;Iph4=0.1*Iph;
 
-U=(0:Uoc/50:2*Uoc)';% To calc
-Ui=(0:Uoc/500:2*Uoc)';% To spline
+U=(0:Uoc/50:Uoc)';% To calc
+Ui=(0:Uoc/500:Uoc)';% To spline
 %% Calc P-V I-V Curve
 U1 = U;
 I = U;
 h = waitbar(0,'Plotting p-v & i-v curves...');
 steps = length(U1);
 for i=1:length(U1)
-    I(i) = Pv4SeriesParallel(U(i),Iph1,Iph2,Iph3,Iph4,I0,N,Rs);
+    I(i) = PvFunctionI(U(i),Iph1,I0,N,Rs);
     waitbar(i/steps);
 end
 close(h);
 %% Spline
 Ii = spline(U,I,Ui);
 %% Plot P-V & I-V Curve
-figure;
-AX=plotyy(Ui,Ii,Ui,Ui.*Ii);
-set(AX(1),'xlim',[0 2*Uoc],'ylim',[0 1.05*max(Ii)],'ytick',0:5*10^floor(log10(max(Ii/10))):1.05*max(Ii));
-set(AX(2),'xlim',[0 2*Uoc],'ylim',[0 1.05*max(Ui.*Ii)],'ytick',0:5*10^floor(log10(max(Ui.*Ii/10))):1.05*max(Ui.*Ii));
+f = figure;
+yyaxis left;
+plot(Ui,Ii);
+ylim([0 1.05*max(Ii)]);
+ylabel('I/A');
+yyaxis right;
+plot(Ui,Ui.*Ii);
+ylim([0 1.05*max(Ui.*Ii)]);
+ylabel('P/W');
+xlim([0 Uoc*1.05]);
+xlabel('U/V');
 hold on;
 %% MPPT
 flag=zeros(length(Ui),1);
@@ -64,23 +71,16 @@ while(1)
            lastk=k;
        end       
     end
-%     yyaxis left;
-%     plot(U,I,'ro');
-%     yyaxis right;
-%     plot(U,U*I,'bo');
-    plotbo =@(X,Y) plot(X,Y,'bo');
-    plotro =@(X,Y) plot(X,Y,'ro');
-    Ax=plotyy(U,I,U,U*I,plotbo,plotro);
-    set(Ax(1),'xlim',[0 2*Uoc],'ylim',[0 1.05*max(Ii)],'ytick',0:5*10^floor(log10(max(Ii/10))):1.05*max(Ii));
-    set(Ax(2),'xlim',[0 2*Uoc],'ylim',[0 1.05*max(Ui.*Ii)],'ytick',[]);
-    %pause(0.1);
+    yyaxis left;
+    plot(U,I,'ko');
+    yyaxis right;
+    plot(U,U*I,'ko');
 end
-% for i=1:length(flag)
-%     if(flag(i)>=4)
-%         yyaxis left;
-%         plot(Ui(i),Ii(i),'r*');
-%         yyaxis right;
-%         plot(Ui(i),Ii(i)*Ui(i),'b*');
-%     end
-% end
-end
+
+title('¾­µäINCËã·¨×·×Ù¹ì¼£');
+grid on;
+grid minor;
+
+saveas(f,'figure/simple-inc.eps','epsc');
+saveas(f,'figure/simple-inc.png');
+close(f);
