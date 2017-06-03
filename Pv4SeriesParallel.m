cf3d83785634edf669b5af306a90de488434b68b
ritou11 <ritou11@gmail.com>
2017-06-03 12:54:20 +0800

    merge deng's report

--- Pv4SeriesParallel.m
+++ Pv4SeriesParallel.m
@@ -1,14 +1,4 @@
 function [I] = Pv4SeriesParallel(U,Iph1,Iph2,Iph3,Iph4,I0,N,Rs)
-% if(Iph1>Iph2)
-%    tmp=Iph1;
-%    Iph1=Iph2;
-%    Iph2=tmp;
-% end
-% if(Iph3>Iph4)
-%    tmp=Iph3;
-%    Iph3=Iph4;
-%    Iph4=tmp;
-% end
 dI=@(u1) PvFunctionI(u1,Iph1,I0,N,Rs)-PvFunctionI(U-u1,Iph2,I0,N,Rs);
 U1=fsolve(dI,U,optimset('Display','off'));
 I=PvFunctionI(U1,Iph1,I0,N,Rs);
